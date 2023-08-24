987698798ce5b58cde3e5b34d80eecc5e3c8e7c4
RicardoVA22 <123458628+RicardoVA22@users.noreply.github.com>
2023-08-24 02:09:34 +0100

    Update merge.sh

--- Merging/merge.sh
+++ Merging/merge.sh
@@ -1,1 +1,56 @@
 
+#!/bin/bash
+#$ -cwd
+#$ -pe smp 28
+#$ -l h_vmem=12G
+#$ -j y
+#$ -l h_rt=148:0:0
+#$ -m bea
+
+#create directory where to store merged files
+CODE_DIR=/data/SBBS-FumagalliLab/Horse/Ricardo/EquSeq
+
+cd $CODE_DIR/results/wgs_data
+mkdir merged
+
+
+DIR=$CODE_DIR/results/wgs_data
+DATA=$DIR/sorted/*.bam
+DATA_FILTERED=$DIR/sorted/*_minq10_sorted.bam
+FILE_OUT=$DIR/merged/ref_merged.bam
+
+#----- load modules ----#
+echo '=================================='
+echo -e "\nLoad samtools\n"
+module load samtools
+
+#----- filter poorly mapped reads----#
+echo '=================================='
+echo -e "\nRemove reads with quality lower than 10\n"
+
+
+#----- merge ----#
+echo '=================================='
+echo -e "\nMerge Files\n"
+samtools merge -f --threads 7 $FILE_OUT $DATA_FILTERED
+
+
+echo '=================================='
+echo -e "\nIndex\n"
+
+samtools index $FILE_OUT $FILE_OUT.bai
+
+
+echo '=================================='
+echo -e "\nflagstat\n"
+
+samtools flagstat $FILE_OUT > $DIR/ref_merged'.stat1.txt'
+
+## clean up memory space
+cd $DIR
+rm -r converted
+
+
+
+
+
